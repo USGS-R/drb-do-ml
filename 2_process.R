dbe0806bf6ff9aaa66786422ece5188fc3722083
Koenig <lkoenigsnyder@usgs.gov>
2021-11-17 17:34:25 -0500

    create table of unique site locations with lat/lon and use to populate DO inventory report

--- 2_process.R
+++ 2_process.R
@@ -1,13 +1,17 @@
 source("2_process/src/filter_wqp_data.R")
-source("2_process/src/munge_cont_DO_data.R")
+source("2_process/src/create_site_list.R")
 
 p2_targets_list <- list(
+  
   # Filter harmonized WQP data for DO data
   tar_target(
-    p2_filtered_wqp_csv,
-    filter_wqp_data(p1_wqp_data,params_select,units_select,select_wqp_vars,omit_wqp_events,fileout="2_process/out/DRB_WQP_DO_data.csv")),
-  # Clean and save continuous DO data 
+    p2_filtered_wqp_data,
+    filter_wqp_data(p1_wqp_data,params_select,units_select,wqp_vars_select,omit_wqp_events)),
+  
+  # Create a list of unique site locations containing DO data  
   tar_target(
-    p2_cont_DO_csv,
-    combine_cont_DO_data(p1_cont_DO_data_ls,fileout="2_process/out/DRB_cont_DO_data.csv"))
+    p2_site_list_csv,
+    create_site_list(p2_filtered_wqp_data,p1_nwis_sites,p1_daily_data,p1_inst_data,fileout = "2_process/out/DRB_DO_sitelist.csv"),
+    format = "file")
+
 )
