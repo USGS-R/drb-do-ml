ad306c289be9982d3d151e659ad14661d8d21e26
Koenig <lkoenigsnyder@usgs.gov>
2021-11-18 08:12:56 -0500

    try .md render in 3_visualize/src

--- 3_visualize.R
+++ 3_visualize.R
@@ -1,6 +1,6 @@
 
 p3_targets_list <- list(
   # Render data summary report
-  tarchetypes::tar_render(p3_wqp_spC_report, "3_visualize/src/report-do-inventory.Rmd",output_dir = "3_visualize/out")
+  tarchetypes::tar_render(p3_wqp_spC_report, "3_visualize/src/report-do-inventory.Rmd",output_dir = "3_visualize/src")
 )
 
