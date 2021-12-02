56121eeac6b6830d3b21244afa4720aeaddb6053
Koenig <lkoenigsnyder@usgs.gov>
2021-12-02 10:37:57 -0500

    aggregate sub-daily DO observations to hourly averages

--- 2_process.R
+++ 2_process.R
@@ -1,6 +1,7 @@
 source("2_process/src/filter_wqp_data.R")
-source("2_process/src/create_site_list.R")
 source("2_process/src/subset_wqp_sites.R")
+source("2_process/src/munge_inst_timeseries.R")
+source("2_process/src/create_site_list.R")
 
 p2_targets_list <- list(
   
@@ -14,10 +15,16 @@ p2_targets_list <- list(
     p2_filtered_wqp_data_subset,
     subset_wqp_sites(p1_lowerdrb_boundary,p2_filtered_wqp_data)),
   
+  # Aggregate instantaneous SC data to hourly averages
+  tar_target(
+    p2_inst_data_hourly,
+    aggregate_data_to_hourly(p1_inst_data,output_tz = "UTC"),
+    pattern = map(p1_inst_data)),
+  
   # Create a list of unique site locations containing DO data  
   tar_target(
     p2_site_list_csv,
-    create_site_list(p2_filtered_wqp_data_subset,p1_nwis_sites,p1_daily_data,p1_inst_data,fileout = "2_process/out/DRB_DO_sitelist.csv"),
+    create_site_list(p2_filtered_wqp_data_subset,p1_lowerdrb_boundary,p1_nwis_sites,p1_daily_data,p2_inst_data_hourly,fileout = "2_process/out/DRB_DO_sitelist.csv"),
     format = "file")
 
 )
