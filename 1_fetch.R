568a59f8dc9fb3f97a7f6bbecdee69b81c350b8e
jsadler2 <jsadler@usgs.gov>
2021-12-10 10:43:32 -0600

    [#16] add stream reach shapefile to pipeline

--- 1_fetch.R
+++ 1_fetch.R
@@ -88,6 +88,31 @@ p1_targets_list <- list(
     p1_inst_timeseries_summary_csv,
     command = target_summary_stats(p1_inst_data,"Value_Inst","1_fetch/log/inst_timeseries_summary.csv"),
     format = "file"
+  ),
+  
+  tar_target(
+    p1_reaches_shp_zip,
+    # [Jeff] I downloaded this manually from science base: 
+    # https://www.sciencebase.gov/catalog/item/5f6a285d82ce38aaa244912e
+    # Because it's a shapefile, it's not easily downloaded using sbtools
+    # like other files are (see https://github.com/USGS-R/sbtools/issues/277).
+    # Because of that and since it's small (<700 Kb) I figured it'd be fine to
+    # just include in the repo and have it loosely referenced to the sb item ^
+    "1_fetch/in/study_stream_reaches.zip",
+    format = "file"
+  ),
+  
+  tar_target(
+    p1_reaches_shp_unzipped,
+    {shapedir = "1_fetch/out/study_stream_reaches"
+    unzip(p1_reaches_shp_zip, exdir = shapedir)},
+    format = "file"
+  ),
+  
+  tar_target(
+    p1_reaches_sf,
+    # third item is the .shp file (all are 'dbf', 'prj', 'shp', and 'shx')
+    st_read(p1_reaches_shp_unzipped[3])
   )
   
 )
