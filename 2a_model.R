4948baad6b401129daddc166f38632f7bd0a0626
jsadler2 <jsadler@usgs.gov>
2021-12-28 14:07:56 -0600

    [#28] write train/validation input/do data to zarr

--- 2a_model.R
+++ 2a_model.R
@@ -1,10 +1,89 @@
 source("2a_model/src/model_ready_data_utils.R")
 
-# subset met data to just well-observed sites and match site ids
 p2a_targets_list <- list(
+  # subset met data to just well-observed sites and match to site_ids
   tar_target(
     p2a_well_observed_met_data,
-    subset_met_data_and_match_site_ids (p1_prms_met_data, p2_sites_w_segs, p2_well_observed_sites)
+    subset_seg_data_and_match_site_ids(p1_prms_met_data, p2_sites_w_segs, p2_well_observed_sites)
+  ),
+
+  # well-observed do data 
+  tar_target(
+    p2a_well_observed_do_data,
+    well_observed_do_data <- p2_daily_with_seg_ids %>%
+                             filter(site_no %in% p2_well_observed_sites) %>%
+                             rename(site_id = site_no,
+                                    date = Date,
+                                    do_mean = Value,
+                                    do_min = Value_Min,
+                                    do_max = Value_Max
+                                    )
+  ),
+
+  tar_target(
+    p2a_well_observed_train_sites,
+    p2_well_observed_sites[!(p2_well_observed_sites %in% validation_sites) & !(p2_well_observed_sites %in% test_sites)]
+  ),
+
+  tar_target(
+    p2a_well_observed_train_validation_sites,
+    p2_well_observed_sites[(p2_well_observed_sites %in% p2a_well_observed_train_sites) | (p2_well_observed_sites %in% validation_sites)]
+  ),
+
+  # match seg attributes with site_ids, subset to train sites and write to feather 
+  tar_target(
+    p2a_well_observed_train_seg_attr,
+    subset_seg_data_and_match_site_ids(p1_seg_attr_data, p2_sites_w_segs, p2a_well_observed_train_sites)
+  ),
+
+  # match seg attributes with site_ids, subset to train and validation sites and write to feather 
+  tar_target(
+   p2a_well_observed_train_validation_seg_attr,
+   subset_seg_data_and_match_site_ids(p1_seg_attr_data, p2_sites_w_segs, p2a_well_observed_train_validation_sites)
+  ),
+
+  # write train met and seg attribute data to zarr
+  tar_target(
+    p2a_train_inputs_zarr,
+    { 
+      train_met <- p2a_well_observed_met_data %>% filter(site_id %in% p2a_well_observed_train_sites)
+      train_input <- train_met %>% left_join(p2a_well_observed_train_seg_attr, by = "site_id")
+      write_df_to_zarr(train_input, c("site_id", "date"), "2a_model/out/well_observed_train_inputs.zarr")
+    },
+    format="file"
+  ),
+
+  # write train and validation met and seg attribute data to zarr
+  tar_target(
+    p2a_train_val_inputs_zarr,
+    { 
+      train_val_met <- p2a_well_observed_met_data %>% filter(site_id %in% p2a_well_observed_train_validation_sites)
+      train_val_input <- train_val_met %>% left_join(p2a_well_observed_train_validation_seg_attr, by = "site_id")
+      write_df_to_zarr(train_val_input, c("site_id", "date"), "2a_model/out/well_observed_train_val_inputs.zarr")
+    },
+    format="file"
+  ),
+
+
+  # write train do data to zarr
+  tar_target(
+    p2a_train_do_zarr,
+    { 
+      train_do <- p2a_well_observed_do_data %>% filter(site_id %in% p2a_well_observed_train_sites)
+      write_df_to_zarr(train_do, c("site_id", "date"), "2a_model/out/well_observed_train_do.zarr")
+    },
+    format="file"
+  ),
+
+  # write train and validation do data to zarr
+  tar_target(
+    p2a_train_validation_do_zarr,
+    { 
+      train_val_do <- p2a_well_observed_do_data %>% filter(site_id %in% p2a_well_observed_train_validation_sites)
+      write_df_to_zarr(train_val_do, c("site_id", "date"), "2a_model/out/well_observed_train_val_do.zarr")
+    },
+    format="file"
   )
+
 
 )
--- 2a_model/src/model_ready_data_utils.R
+++ 2a_model/src/model_ready_data_utils.R
@@ -1,29 +1,53 @@
 
 
-subset_met_data_and_match_site_ids <- function(met_data, sites_w_segs, sites_subset = NULL){
-  #' 
-  #' @description match site ids to meterological data and optionally subset to a certain list of sites
-  #'
-  #' @param met_data a data frame of meterological data indexed to seg_id_nats
-  #' @param sites_w_segs a dataframe with both segment ids ('segidnat') and site ids ('site_id')
-  #' @param sites_subset a vector of sites that the data should be subset too. If left NULL, data from all sites will 
-  #' be returned
-  #' 
-  #' @value A data frame of met data with site ids 
-
+subset_seg_data_and_match_site_ids <-
+  function(seg_data, sites_w_segs, sites_subset = NULL) {
+    #'
+    #' @description match site ids to segment data (e.g., met or attributes) and optionally subset to a certain list of sites
+    #'
+    #' @param seg_data a data frame of meterological data with column 'seg_id_nat'
+    #' @param sites_w_segs a dataframe with both segment ids ('segidnat') and site ids ('site_id')
+    #' @param sites_subset a vector of sites that the data should be subset too. If left NULL, data from all sites will
+    #' be returned
+    #'
+    #' @value A data frame of seg data with site ids and optionally subset
+    
     seg_and_site_ids <- sites_w_segs %>% select(site_id, segidnat)
-
-
-    met_data <- met_data %>%
-      left_join(
-        seg_and_site_ids,
-        by = c("seg_id_nat" = "segidnat")
-      )
-
-    if (!is.null(sites_w_segs)){
-      met_data <- met_data %>% filter(site_id %in% sites_subset)
+    
+    
+    seg_data <- seg_data %>%
+      left_join(seg_and_site_ids,
+                by = c("seg_id_nat" = "segidnat"))
+    
+    if (!is.null(sites_w_segs)) {
+      seg_data <- seg_data %>% filter(site_id %in% sites_subset)
     }
+    
+    return(seg_data)
+  }
 
-    return(met_data)
+write_df_to_zarr <- function(df, index_cols, out_zarr) {
+  #'
+  #' @description use reticulate to write an R data frame to a Zarr data store (the file format river-dl currently takes)
+  #'
+  #' @param df a data frame of data
+  #' @param index vector of strings - the column(s) that should be the index
+  #' @param out_zarr where the zarr data will be written
+  #'
+  #' @value the out_zarr path
+  
+  # convert to a python (pandas) DataFrame so we have access to the object methods (set_index and to_xarray)
+  py_df <- reticulate::r_to_py(df)
+  
+  # set the index so that when we convert to an xarray dataset it is indexed properly
+  py_df  <- py_df$set_index(index_cols)
+  
+  # convert to an xarray dataset
+  ds <- py_df$to_xarray()
+  
+  ds$to_zarr(out_zarr, mode = 'w')
+  
+  return(out_zarr)
+  
 }
 
