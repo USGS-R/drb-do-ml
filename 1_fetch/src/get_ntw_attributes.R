ada465929ceb96a3e35f09a7aff8facffd9c43d3
jsadler2 <jsadler@usgs.gov>
2021-12-28 10:14:11 -0600

    [#17] minor corrections in documentation

--- 1_fetch/src/get_ntw_attributes.R
+++ 1_fetch/src/get_ntw_attributes.R
@@ -5,9 +5,9 @@ get_ntw_attributes <- function(save_path){
   #'
   #' @value returns a character string indicating the location of the downloaded network attributes
   #' @examples 
-  #' get_ntw_attributes(save_path = "1_fetch/out/ntw_attributes)
+  #' get_ntw_attributes(save_path = "1_fetch/out/ntw_attributes")
   
-  # Data release, "Predicting temperature in the Delaware River Basin, Model Inputs" (https://www.sciencebase.gov/catalog/item/5f6a289982ce38aaa2449135)
+  #' Data release, "Predicting temperature in the Delaware River Basin, Model Inputs" (https://www.sciencebase.gov/catalog/item/5f6a289982ce38aaa2449135)
   sb_item_id <- "5f6a289982ce38aaa2449135"
   
   # Download model input files to save_path
