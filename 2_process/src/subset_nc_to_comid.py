26d14ebab573353fe30ba20ce58ec34a578d925e
jsadler2 <jsadler@usgs.gov>
2022-06-17 10:20:06 -0500

    [#131] trying to make the subset faster

--- 2_process/src/subset_nc_to_comid.py
+++ 2_process/src/subset_nc_to_comid.py
@@ -1,5 +1,22 @@
 import xarray as xr
 import numpy as np
+import pandas as pd
+
+
+def ds_to_dataframe_faster(ds):
+    """
+    doing this to try to avoid the multi-index joins
+    """
+    series_list = []
+    for var in ds.data_vars:
+        if "lat" not in var and "lon" not in var:
+            df_var = ds[var].to_pandas().reset_index()
+            df_var_tidy = df_var.melt(id_vars='COMID', value_name=var)
+            series_list.append(df_var_tidy[var])
+    series_list.append(df_var_tidy['COMID'])
+    series_list.append(df_var_tidy['time'])
+    return pd.concat(series_list, axis=1)
+
 
 def subset_nc_to_comids(nc_file, comids):
     comids = [int(c) for c in comids]
@@ -12,7 +29,7 @@ def subset_nc_to_comids(nc_file, comids):
     comids_not_in_climate = comids[~np.isin(comids, ds.COMID.values)]
 
     # make sure it's just the one that we are expecting
-    assert list(comids_not_in_climate) == [4781767]
-
+    if len(comids_not_in_climate) > 0 :
+        assert list(comids_not_in_climate) == [4781767]
     ds_comids = ds.sel(COMID=comids_in_climate)
-    return ds_comids.to_dataframe()
+    return ds_to_dataframe_faster(ds_comids)
