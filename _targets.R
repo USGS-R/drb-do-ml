f5bba074d4a1af847a4cfb8fc2dae85e1e12bd83
Koenig <lkoenigsnyder@usgs.gov>
2022-10-20 09:20:23 -0400

    initialize subdir to save nhd attr plots

--- _targets.R
+++ _targets.R
@@ -18,6 +18,7 @@ dir.create("2_process/out/", showWarnings = FALSE)
 dir.create("2a_model/out/", showWarnings = FALSE)
 dir.create("2_process/log/", showWarnings = FALSE)
 dir.create("3_visualize/out/", showWarnings = FALSE)
+dir.create("3_visualize/out/nhdv2_attr_png/", showWarnings = FALSE)
 dir.create("3_visualize/log/", showWarnings = FALSE)
 
 # Define columns of interest from harmonized WQP data
